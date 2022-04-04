89f4b862faaff8cad72f278b1bd10f6d03e42741
mygirok <forgirok@gmail.com>
2022-04-04 20:59:09 +0900

    Add player moving

--- mazeGame/mazeGame.cpp
+++ mazeGame/mazeGame.cpp
@@ -82,6 +82,78 @@ void Output(char Maze[21][21], PPOINT pPlayerPos)
 	}
 }
 
+void MoveUp(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->y - 1 >= 0)
+	{
+		// check wall
+		if (Maze[pPlayerPos->y - 1][pPlayerPos->x] != '0')
+		{
+			--pPlayerPos->y;
+		}
+	}
+}
+
+void MoveDown(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->y + 1 < 20)
+	{
+		// check wall
+		if (Maze[pPlayerPos->y + 1][pPlayerPos->x] != '0')
+		{
+			++pPlayerPos->y;
+		}
+	}
+}
+
+void MoveRight(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->x + 1 < 20)
+	{
+		// check wall
+		if (Maze[pPlayerPos->y][pPlayerPos->x + 1] != '0')
+		{
+			++pPlayerPos->x;
+		}
+	}
+}
+
+void MoveLeft(char Maze[21][21], PPOINT pPlayerPos)
+{
+	if (pPlayerPos->x - 1 < 20)
+	{
+		// check wall
+		if (Maze[pPlayerPos->y][pPlayerPos->x - 1] != '0')
+		{
+			--pPlayerPos->x;
+		}
+	}
+}
+
+
+void MovePlyaer(char Maze[21][21], PPOINT pPlayerPos, char cInput)
+{
+	switch (cInput)
+	{
+	case 'w':
+	case 'W':
+		MoveUp(Maze, pPlayerPos);
+		break;
+	case 's':
+	case 'S':
+		MoveDown(Maze, pPlayerPos);
+		break;
+	case 'a':
+	case 'A':
+		MoveLeft(Maze, pPlayerPos);
+		break;
+	case 'd':
+	case 'D':
+		MoveRight(Maze, pPlayerPos);
+		break;
+	}
+}
+
 int main()
 {
 	// 20 X 20 maze
@@ -104,6 +176,8 @@ int main()
 
 		if (cInput == 'q' || cInput == 'Q')
 			break;
+
+		MovePlyaer(strMaze, &tPlayerPos, cInput);
 	}
 
 	return 0;
